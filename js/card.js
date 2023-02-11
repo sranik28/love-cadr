062e84303d583ef5c2d4fb98d67d7c3b2980f301
sranik28~ <anikshariarrahman@gmail.com>
2023-02-11 21:20:35 +0600

    add bgcolor

--- js/card.js
+++ js/card.js
@@ -1,7 +1,17 @@
 document.getElementById('first_button').addEventListener('click', function(){
-document.body.style.backgroundColor = "pink";
-document.getElementById('main-text').innerText = "I Love You Too"
+document.body.style.backgroundImage = "url('../images/love.png')";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundPosition = 'right' ;
+
+
+document.getElementById('main-text').innerText = "I Love You Too";
+
+document.getElementById('main-text').style.color = 'pink';
+
 })
 document.getElementById('secound-button').addEventListener('click', function(){
-    document.getElementById('main-text').innerText = 'Sorry I have A boyfriend'
+    document.getElementById('main-text').innerText = 'Sorry I Have A Boyfriend';
+    document.body.style.backgroundImage = "url('../images/sad.jpg')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundPosition = 'top' ;
 })
