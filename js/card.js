6b242b11d9da0ad4b5b1ec45402826638fdb74e6
sranik28~ <anikshariarrahman@gmail.com>
2023-02-11 20:06:31 +0600

    first

---
+++ js/card.js
@@ -0,0 +1,7 @@
+document.getElementById('first_button').addEventListener('click', function(){
+document.body.style.backgroundColor = "pink";
+document.getElementById('main-text').innerText = "I Love You Too"
+})
+document.getElementById('secound-button').addEventListener('click', function(){
+    document.getElementById('main-text').innerText = 'Sorry I have A boyfriend'
+})
